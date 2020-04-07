2cbbfb8d85b1afc5776376172d1b553a277136df
Salvador <salvador.on@outlook.com>
2020-04-07 13:23:24 -0300

    add rspec for views

--- spec/views/groups_spec.rb
+++ spec/views/groups_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe 'Transaction Views', type: :feature do
+RSpec.describe 'Groups Views', type: :feature do
   before(:each) do
     u1=User.new({email:'test1@s.com', name:'Test1', password:'123456', password_confirmation:'123456'})
     u1.avatar=Pathname.new(Rails.root.join("app/assets/images/actor_avatar_people_person_profile_user_voice_icon_123373.png")).open
@@ -14,43 +14,14 @@ RSpec.describe 'Transaction Views', type: :feature do
     expect(page).to have_content('Test1')
   end
 
-  scenario 'visit my expenses' do
-    find('a', text: 'My expenses').click
-    expect(page).to have_content('Expenses')
-  end
 
-  scenario 'visit none categorised expenses' do
-    find('a', text: 'None categorised expenses').click
-    expect(page).to have_content('None Categorised')
-  end
-
-  scenario 'visit categories' do
+  scenario 'add new categorie' do
     find('a', text: 'Categories').click
+    find(:xpath, "//a[@href='/groups/new']").click
+    fill_in 'Name', with: 'test'
+    choose(option: 'fa-glass-cheers')
+    click_button('Create Group')
     expect(page).to have_content('Categories')
-  end
-
-  scenario 'visit members expenses' do
-    find('a', text: 'Members expenses').click
-    expect(page).to have_content('Members Expenses')
-  end
-
-  scenario 'create new categorise expense' do
-    find('a', text: 'My expenses').click
-    find(:xpath, "//a[@href='/transactions/new']").click
-    fill_in 'Name', with: 'test'
-    fill_in 'Amount', with: '123'
-    find('#transaction_group_id').find(:xpath, 'option[2]').select_option
-    click_button('Create Transaction')
-    expect(page).to have_content('Expenses')
-  end
-
-  scenario 'create new none categorise expense' do
-    find('a', text: 'None categorised expenses').click
-    find(:xpath, "//a[@href='/transactions/new']").click
-    fill_in 'Name', with: 'test'
-    fill_in 'Amount', with: '123'
-    click_button('Create Transaction')
-    expect(page).to have_content('None Categorised')
   end
 
   
