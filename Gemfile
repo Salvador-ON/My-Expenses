72916b2f783dd92fe7c69b6b5c0a064b5a15a4b5
Salvador <salvador.on@outlook.com>
2020-04-07 13:08:23 -0300

    add rspec for models

--- Gemfile
+++ Gemfile
@@ -71,9 +71,9 @@ end
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
-  gem 'selenium-webdriver'
+  gem 'webdrivers', '~> 3.0'
   # Easy installation and use of chromedriver to run system tests with Chrome
-  gem 'chromedriver-helper'
+  #gem 'chromedriver-helper'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    u1=User.new({email:'test1@s.com', name:'Test1', password:'123456', password_confirmation:'123456'})
+    u1.avatar=Pathname.new(Rails.root.join("app/assets/images/actor_avatar_people_person_profile_user_voice_icon_123373.png")).open
+    u1.save
+    g1=Group.new(name:'testgroup 1', icon:'fa-handshake', user_id:1)
+    g1.save
+  end
+
+  context 'test transaction model' do
+    it 'return true if created transaction is valid' do
+      e1=Transaction.new(name:'Food', amount:25, user_id:1, group_id:1)
+      e1.save
+      expect(e1.valid?).to eq(true)
+    end
+
+    it 'return true if created transaction is valid without group' do
+      e1=Transaction.new(name:'Food', amount:25, user_id:1)
+      e1.save
+      expect(e1.valid?).to eq(true)
+    end
+
+    it 'return false if created transaction is invalid missing name' do
+      e1=Transaction.new(amount:25, user_id:1, group_id:1)
+      e1.save
+      expect(e1.valid?).to eq(false)
+    end
+
+    it 'return false if created transaction is invalid missing amount' do
+      e1=Transaction.new(name:'Food', user_id:1, group_id:1)
+      e1.save
+      expect(e1.valid?).to eq(false)
+    end
+
+  end
 end
